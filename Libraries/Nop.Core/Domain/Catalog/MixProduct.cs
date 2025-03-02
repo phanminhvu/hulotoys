73c87b72a763de67a7267aa1e2d609d3929c7480
TungNV <vantungitss@gmail.com>
2025-03-03 00:23:29 +0700

    add MixProduct

---
+++ Libraries/Nop.Core/Domain/Catalog/MixProduct.cs
@@ -0,0 +1,19 @@
+﻿using Nop.Core.Domain.Common;
+
+namespace Nop.Core.Domain.Catalog;
+
+/// <summary>
+/// Sản phẩm kết hợp
+/// </summary>
+public partial class MixProduct : BaseEntity, ISoftDeletedEntity
+{
+    /// <summary>
+    /// Danh sách các sản phẩm thành phần
+    /// </summary>
+    public string ProductIds { get; set; }
+    /// <summary>
+    /// Trang thái xoá
+    /// </summary>
+    public bool Deleted { get; set; }
+
+}
---
+++ Libraries/Nop.Data/Mapping/Builders/Catalog/MixProductBuilder.cs
@@ -0,0 +1,25 @@
+﻿using FluentMigrator.Builders.Create.Table;
+using Nop.Core.Domain.Catalog;
+
+namespace Nop.Data.Mapping.Builders.Catalog;
+
+/// <summary>
+/// Represents a category entity builder
+/// </summary>
+public partial class MixProductBuilder : NopEntityBuilder<MixProduct>
+{
+    #region Methods
+
+    /// <summary>
+    /// Apply entity configuration
+    /// </summary>
+    /// <param name="table">Create table expression builder</param>
+    public override void MapEntity(CreateTableExpressionBuilder table)
+    {
+        table
+                .WithColumn(nameof(MixProduct.ProductIds)).AsString(int.MaxValue).NotNullable()
+                .WithColumn(nameof(MixProduct.Deleted)).AsBoolean().WithDefaultValue(false);
+    }
+
+    #endregion
+}
